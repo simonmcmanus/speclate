8d5a183eff9a801213d24ccaa1496b41da8d08c6
Codeship CI <mcmanus.simon@gmail.com>
2015-11-26 17:46:54 +0000

    pages can be function allowing for markdown pages

--- client/index.js
+++ client/index.js
@@ -9,7 +9,6 @@ window.setupPage = function(pageName, context, page) {
     $.get(context.path.replace(/.html/, '.json'), {}, function(e, status, data) {
         var pageLayoutPath =  '/public//pages/' + pageName + '/' + pageName + '.html';
         getFile(pageLayoutPath, function(error, file) {
-            console.log('spec is', data.responseJSON.spec);
             loadComponents(data.responseJSON.spec, function(e,d) {
                 var html = doSizlate({
                     spec: page.spec
