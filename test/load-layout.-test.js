d7b81ea0d6f9b011bfb9133269c30b9d75aaec2f
Codeship CI <mcmanus.simon@gmail.com>
2016-02-10 20:01:12 +0000

    added tests for load-component and load-layout

--- test/load-layout.-test.js
+++ test/load-layout.-test.js
@@ -1,9 +1,7 @@
 var expect = require('chai').expect;
-
-
 var loadLayout = require('../lib/load-layout');
 
-describe('Given a layout ', () => {
+describe('When load layout is called requesting the animals layout ', () => {
     var layout = 'animals';
     var markup = '';
     beforeEach((next) => {
@@ -11,9 +9,8 @@ describe('Given a layout ', () => {
             markup = data;
             next();
         });
-    })
-    it('should do stuff', () => {
-        expect(markup).to.equal('');
-    })
-
+    });
+    it('should load the animals template into the main layout into the container div', () => {
+        expect(markup).to.equal("<html>\n<body>\n    <div id=\"container\"><ul id=\"animals\">\n</ul>\n</div>\n</body>\n</html>\n");
+    });
 });
