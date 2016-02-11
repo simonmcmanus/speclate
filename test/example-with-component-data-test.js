b3623af97c0a37c50676ba78587d8927c2a697ad
Codeship CI <mcmanus.simon@gmail.com>
2016-02-11 19:46:19 +0000

    1 failin

--- test/example-with-component-data-test.js
+++ test/example-with-component-data-test.js
@@ -1,33 +1,38 @@
 var loader = require('../lib/loader');
 var expect = require('chai').expect;
 
-describe('given a simple index.html page', () => {
-    var spec;
-    var out;
-    before((next) => {
-        var spec = {
-            'index.html': {
-                page: 'home',
-                spec: {
-                    '#bacon': {
-                        component: 'cat',
-                        data: [{
-                            img: {
-                                src: 'IMGPATH'
-                            }
-                        }]
+
+describe('example / component - data /', () => {
+
+    describe('given a simple index.html page', () => {
+        var spec;
+        var out;
+        before((next) => {
+            var spec = {
+                'index.html': {
+                    page: 'home',
+                    spec: {
+                        '#bacon': {
+                            component: 'cat',
+                            data: [{
+                                img: {
+                                    src: 'IMGPATH'
+                                }
+                            }]
+                        }
                     }
                 }
-            }
-        };
-        loader(spec, (e, d) => {
-            console.log(d)
-            out = d;
-            next();
-        });
-    })
+            };
+            loader(spec, (e, d) => {
+                console.log(d)
+                out = d;
+                next();
+            });
+        })
 
-    it('should add the component to the appropriate place in the DOM', () => {
-        expect(out[0].markup).to.equal('<html>\n<div id=\"container\"><h1>Homepage</h1>\n<div id=\"bacon\"><li>\n    Hello i am a cat.\n</li>\n<img src=\"IMGPATH\">\n</div>\n</div>\n</html>\n');
-    })
+        it('should add the component to the appropriate place in the DOM', () => {
+            expect(out[0].markup).to.equal('<html>\n<div id=\"container\"><h1>Homepage</h1>\n\n<div id=\"bacon\"><li>\n    Hello i am a cat.\n</li>\n<img src=\"IMGPATH\">\n</div></div>\n</html>\n');
+        })
+    });
+
 });
--- test/example-with-component-test.js
+++ test/example-with-component-test.js
@@ -1,27 +1,31 @@
 var loader = require('../lib/loader');
 var expect = require('chai').expect;
 
-describe('given a simple index.html page', () => {
-    var spec;
-    var out;
-    before((next) => {
-        var spec = {
-            'index.html': {
-                page: 'home',
-                spec: {
-                    '#bacon': {
-                        component: 'cat'
+
+describe('example/component', () => {
+    describe('given a simple index.html page', () => {
+        var spec;
+        var out;
+        before((next) => {
+            var spec = {
+                'index.html': {
+                    page: 'home',
+                    spec: {
+                        '#bacon': {
+                            component: 'cat'
+                        }
                     }
                 }
-            }
-        };
-        loader(spec, (e, d) => {
-            out = d;
-            next();
-        });
-    })
+            };
+            loader(spec, (e, d) => {
+                out = d;
+                next();
+            });
+        })
 
-    it('should add the component to the appropriate place in the DOM', () => {
-        expect(out[0].markup).to.equal('<html>\n<div id=\"container\"><h1>Homepage</h1>\n<div id=\"bacon\">\n</div>\n</div>\n</html>\n');
-    })
+        it('should add the component to the appropriate place in the DOM', () => {
+            expect(out[0].markup).to.equal('<html>\n<div id=\"container\"><h1>Homepage</h1>\n\n<div id=\"bacon\">\n</div></div>\n</html>\n');
+        })
+    });
+
 });
