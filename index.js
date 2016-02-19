f1a841316a87519bbbef2872056a1a403aae5d8c
Codeship CI <mcmanus.simon@gmail.com>
2016-02-19 16:12:46 +0000

    exposing pageify

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 exports.generate = require('./lib/generate-site')
 exports.api = require('./lib/generate-api')
-// exports.pageify = require('./lib/pageify'); // generate pagify router from a spec
+exports.pageify = require('./lib/pageify'); // generate pagify router from a spec
 exports.appCache = require('./lib/app-cache')
