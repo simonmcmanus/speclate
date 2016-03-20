6c7570028dc0ed456d59a5c79f743e24d46269f9
Codeship CI <mcmanus.simon@gmail.com>
2016-03-20 20:47:05 +0000

    adding test for selectors

---
+++ test/example-top-level-selectors.js
@@ -0,0 +1,30 @@
+var loader = require('../lib/loader')
+var expect = require('chai').expect
+
+describe('example / toplevel selectors', () => {
+  describe('given a simple index.html page', () => {
+    var out
+    before((next) => {
+      var spec = {
+        'index.html': {
+          page: 'home',
+          selectors: {
+            '#container': 'HTMLOVERRIDE'
+          }
+        }
+      }
+      loader(spec, (e, d) => {
+        out = d
+        next()
+      })
+    })
+
+    it('the first item returned should have a name property of index.html', () => {
+      expect(out[0].name).to.equal('index.html')
+    })
+
+    it('the first item returned should have a markup property containing the outer layout and the page layout ', () => {
+      expect(out[0].markup).to.equal('<html>\n<div id=\"container\">HTMLOVERRIDE</div>\n</html>\n')
+    })
+  })
+})
