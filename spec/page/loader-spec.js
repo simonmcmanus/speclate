7d36d3ef2dd56aac2f8e4a0ce3cf122c0836b634
Simon McManus <mcmanus.simon@gmail.com>
2016-07-02 23:14:26 +0200

    adding some tests which can be run on the client
    more changes

--- spec/page/loader-spec.js
+++ spec/page/loader-spec.js
@@ -1,55 +1,113 @@
-if(typeof require != 'undefined') {
-    var speclate = require('../../');
-}
+
+var speclate = require('../../');
+
+describe('page - loader', () => {
+
+    var pageSpec;
+    var out;
+
+    describe('Given a simple spec which only defines the page', () => {
+
+            beforeEach((next) => {
+                var pageSpec = {
+                    page: 'home'
+                }
+                speclate.page.load(pageSpec, (err, data) => {
+                    out = data;
+                    next();
+                });
+            })
+
+            it('should render the page in the container', () => {
+                expect(out).toEqual('<div id="container"><div>\n    <h1>Homepage</h1>\n<div id="bacon">\n</div>\n</div></div>');
+            });
+    });
 
 
-var sinon = require('sinon');
+    describe('Given a page spec which defines selectors', () => {
 
-
-describe(' ', () => {
-
-    var markup;
-    var pageSpec = {
-        page: 'home',
-        spec: {
-            '.className': {
-                component: 'sponsor',
-                data: {}
+        beforeEach((next) => {
+            var pageSpec = {
+                page: 'home',
+                selectors: {
+                    h1: 'welcome'
+                }
             }
-        }
-    };
-    beforeEach(() => {
-        speclate.page.load(pageSpec, (err, data) => {
-            markup = data;
+            speclate.page.load(pageSpec, (err, data) => {
+                out = data;
+                next();
+            });
+        })
+
+        it('should update the h1', () => {
+            expect(out).toContain('<h1>welcome</h1>');
         });
-    });
-    it('should load the animals template into the main layout into the container div', () => {
-        expect(markup).toEqual("<html>\n<body>\n    <div id=\"container\"><ul id=\"animals\">\n</ul>\n</div>\n</body>\n</html>\n");
-    });
+    })
+
+
+    describe('Given a page spec which defines a title selector', () => {
+
+        beforeEach((next) => {
+            var pageSpec = {
+                page: 'home',
+                selectors: {
+                    title: 'welcome'
+                }
+            }
+            speclate.page.load(pageSpec, (err, data) => {
+                out = data;
+                next();
+            });
+        })
+
+        //it('should update the page title', () => {
+        //    expect(out).toEqual('<div id="container"><div>\n    <h1>welcome</h1>\n<div id="bacon">\n</div>\n</div></div>');
+        //});
+    })
+
+    // describe('Given a page spec which specifies a component and some data', () => {
+
+    //     beforeEach((next) => {
+    //         var pageSpec = {
+    //             page: 'home',
+    //             spec: {
+    //                 component: 'cat'
+    //                 data: {
+    //                     li: 'hello kitty'
+    //                 }
+    //             }
+    //         }
+    //         speclate.page.load(pageSpec, (err, data) => {
+    //             out = data;
+    //             next();
+    //         });
+    //     })
+
+    //     // it('should update the h1', () => {
+    //     //     expect(out).toContain('<h1>welcome</h1>');
+    //     // });
+    // })
+
+
+    describe('Given a page spec which the page as a function', () => {
+
+        beforeEach((next) => {
+            var pageSpec = {
+                page: function(params, callback) {
+                    callback('<div> here is your page </div>');
+                }
+            }
+            speclate.page.load(pageSpec, (err, data) => {
+                out = data;
+                next();
+            });
+        })
+
+        // it('should use the template provided by the function', () => {
+        //     expect(out).toContain('<h1>welcome</h1>');
+        // });
+    })
+
 });
 
 
-// var expect = require('chai').expect;
-// var getPage = require('../lib/get-page');
-//
-// describe('given a page', function() {
-//     var pageSpec;
-//     beforeEach(() => {
-//         pageSpec = {
-//             page: 'home',
-//             spec: {
-//                 '.className': {
-//                     component: 'sponsor',
-//                     data: {}
-//                 }
-//             }
-//         };
-//     });
-//
-//     getPage();
-//
-//     it('shoud', () => {
-//         expect(1).to.equal(1)
-//     })
-//
-// });
