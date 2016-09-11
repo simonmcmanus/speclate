27d1a924f64d848551620ed04ccd265b52589b93
Simon McManus <mcmanus.simon@gmail.com>
2016-09-11 14:29:21 +0100

    send version to speclate cli

--- bin/speclate.js
+++ bin/speclate.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-
+var package = require(__dirname + '/../package.json');
 var spec = require(process.cwd() + '/spec.js');
 
-require('speclate-cli')(spec);
+require('speclate-cli')(spec, package.version);
