f26b997698f452cdbe19e8bcabfed25ba9bb1611
Simon McManus <mcmanus.simon@gmail.com>
2016-09-11 16:14:53 +0100

    pass speclate into speclate-cli

--- bin/speclate.js
+++ bin/speclate.js
@@ -2,5 +2,6 @@
 var path = require('path')
 var pkg = require(path.join(__dirname, '/../package.json'))
 var spec = require(process.cwd() + '/spec.js')
+var speclate = require('../')
 
-require('speclate-cli')(spec, pkg.version)
+require('speclate-cli')(spec, speclate, pkg.version)
