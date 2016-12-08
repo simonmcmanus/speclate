df32b0d9130bed535e4d2aceb3f4d111ca8d2276
Simon McManus <mcmanus.simon@gmail.com>
2016-12-08 21:04:09 +0000

    speclate-cli@2.x.x

--- bin/speclate.js
+++ bin/speclate.js
@@ -2,6 +2,5 @@
 var path = require('path')
 var pkg = require(path.join(__dirname, '/../package.json'))
 var spec = require(process.cwd() + '/spec.js')
-var speclate = require('../')
 
-require('speclate-cli')(spec, speclate, pkg.version)
+require('speclate-cli')(spec, pkg.version)
