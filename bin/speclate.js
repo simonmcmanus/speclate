d6f1fa1922816864611cc6694effd4497cf767af
Simon McManus <mcmanus.simon@gmail.com>
2017-01-15 19:35:25 +0000

    handle error in callback

--- bin/speclate.js
+++ bin/speclate.js
@@ -3,4 +3,13 @@ var path = require('path')
 var pkg = require(path.join(__dirname, '/../package.json'))
 var spec = require(process.cwd() + '/spec.js')
 
-require('speclate-cli')(spec, pkg.version)
+var speclateCli = require('speclate-cli');
+
+speclateCli(spec, pkg.version, function (err) {
+  if (err) {
+    console.log('got an error')
+    process.exit(1)
+  } else {
+    process.exit(0)
+  }
+});
