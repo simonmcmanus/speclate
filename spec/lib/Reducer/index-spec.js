11d11b37f6cce8cd84337b44dc15cf249f474916
Simon McManus <mcmanus.simon@gmail.com>
2019-01-01 15:25:47 +0000

    working serverside but not client side

---
+++ spec/lib/Reducer/index-spec.js
@@ -0,0 +1,20 @@
+
+
+describe('Reducer', function () {
+
+  describe('Given a simple reducer', function () {
+
+
+    describe('Calling `Get`', () => {
+
+    })
+
+    it('should render the data', function () {
+      //expect(out).toContain('<div id="container"><h1>Homepage</h1>\n<div id="bacon">\n</div>\n</div>')
+    })
+  })
+
+  describe('Given a reducer with a groupBy method', () => {
+    it('should ')
+  })
+})
