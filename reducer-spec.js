8c6e3702a6ab3a1261853028eaa68b7e2da7974f
Simon McManus <mcmanus.simon@gmail.com>
2019-01-01 14:25:12 +0000

    adding reducer

--- reducer-spec.js
+++ reducer-spec.js
@@ -1,75 +1,106 @@
-// experiments
+// // experiments
 
-var breakfast = [
-  {
-    id: 1,
-    title: 'bacon',
-    summary: '2 rashes of bacon'
-  },
-  {
-    id: 2,
-    title: 'cheese',
-    summary: 'four slabs of cheese'
-  }
-]
+// var breakfast = [
+//   {
+//     id: 1,
+//     title: 'bacon',
+//     summary: '2 rashes of bacon'
+//   },
+//   {
+//     id: 2,
+//     title: 'cheese',
+//     summary: 'four slabs of cheese'
+//   }
+// ]
 
-var lunch = [
-  {
-    id: 3,
-    title: 'pizza',
-    summary: '3 pizzas'
-  },
-  {
-    id: 4,
-    title: 'ice cream',
-    summary: '3 cones of ice cream'
-  }
-]
+// var lunch = [
+//   {
+//     id: 3,
+//     title: 'pizza',
+//     summary: '3 pizzas'
+//   },
+//   {
+//     id: 4,
+//     title: 'ice cream',
+//     summary: '3 cones of ice cream'
+//   }
+// ]
 
-const Reducer = (datas, options) => {
-  this.sources = datas
-  this.combined = [].concat.apply(true, datas).slice(1)
-  this.options = options
+// const Reducer = (datas, options) => {
+//   this.sources = datas
+//   this.combined = [].concat.apply(true, datas).slice(1)
+//   this.options = options
 
-  if (typeof this.options.sort === 'function') {
-    this.combined = this.options.sort(this.options)
-  }
+//   if (typeof this.options.sort === 'function') {
+//     this.combined = this.options.sort(this.options)
+//   }
 
-  this.get = () => {
-    var out = this.combined
+//   this.get = () => {
+//     var out = this.combined
 
-    options.filters.forEach((filter) => {
-      out = out.filter(filter)
-    })
-    options.maps.forEach((map) => {
-      out = out.map(map)
-    })
-    return out
-  }
-  return this
-}
+//     options.filters.forEach((filter) => {
+//       out = out.filter(filter)
+//     })
+//     options.maps.forEach((map) => {
+//       out = out.map(map)
+//     })
+//     return out
+//   }
+//   this.groupBy = (field) => {
+//     if (typeof field === 'function') {
+//       //
+//     }
+//   }
+//   return this
+// }
 
-var food = Reducer([breakfast, lunch], {
-  maps: [
-    (item) => {
-      return {
-        title: item.title
-      }
-    },
-    (item) => {
-      return {
-        '.title': item.title
-      }
-    }
-  ],
-  filters: [
-    (item) => {
-      return item.title !== 'pizza'
-    },
-    (item) => {
-      return item.title !== 'cheese'
-    }
-  ]
-})
+// var food = Reducer([breakfast, lunch], {
+//   maps: [
+//     (item) => ({
+//       title: item.title
+//     })
+//   ],
+//   selectors: (item) => ({
+//     '.title': item.title
+//   }),
+//   filters: [
+//     (item) => item.title !== 'pizza',
+//     (item) => item.title !== 'cheese'
+//   ]
+// })
 
-console.log(food.get())
+// Reducer([breakfast, lunch], {
+//   maps: [
+//     (item) => ({
+//       title: item.title
+//     })
+//   ],
+//   selectors: (item) => ({
+//     '.title': item.title
+//   }),
+//   filters: [
+//     (item) => item.title !== 'pizza',
+//     (item) => item.title !== 'cheese'
+//   ]
+// })
+
+// // var posts = Reducer([breakfast, lunch], {
+// //   maps: {
+// //     summary: (post) => ({
+// //       title: post.title
+// //     }),
+// //     post: (post) => ({
+
+// //     })
+// //   },
+// //   selectors: (item) => ({
+// //     '.title': item.title,
+// //     '.summary': item.summary
+// //   }),
+// //   filters: [
+// //     (item) => item.title !== 'pizza',
+// //     (item) => item.title !== 'cheese'
+// //   ]
+// // })
+
+// console.log(food.get())
