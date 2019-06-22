b3bf00326544ed78617efa7d9197eacaf0ea9e9d
Simon McManus <mcmanus.simon@gmail.com>
2019-06-22 09:52:09 +0100

    all tests passing page as a fun and we need more e2e tests

--- router/tests/sample/client/router-compiled.js
+++ router/tests/sample/client/router-compiled.js
@@ -67,20 +67,13 @@ module.exports = function (page, layout, renderedComponents) {
   }
 
   var out = sizlate.render(layout, componentSelectors)
-  console.log('out', out)
   if (typeof document === 'undefined') {
     simpleSelectors.html = {
       'data-speclate-page': page.page,
       'data-speclate-url': page.route || 'unknown'
     }
     // if serverside user the existing page.
-  } else {
-    // if clientside user the domNode passed in.
-    console.log('is client')
   }
-
-  console.log('out', simpleSelectors)
-
   return sizlate.render(out, simpleSelectors)
 }
 
@@ -1115,6 +1108,7 @@ module.exports = function (oldValue, newValue) {
     if (typeof newValue === 'object' && newValue.regex && newValue.value) {
         return oldValue.replace(newValue.regex, newValue.value);
     } else if (typeof newValue === 'function') {
+        console.log('new value is func')
         return newValue(oldValue);
     }
     return newValue;
