24a3dcac553657e9a3344731273bdecfadc2fce4
Simon McManus <mcmanus.simon@gmail.com>
2020-02-01 06:24:57 +0000

    basic load components spec

--- tests/spec/lists/load-components.spec.js
+++ tests/spec/lists/load-components.spec.js
@@ -1,57 +1,55 @@
-var loadComponents = require("../../../lib/page/load-components");
+var loadComponents = require('../../../lib/page/load-components')
 
-describe("when render components is called", () => {
+describe('when render components is called', () => {
   var page = {
-    page: "links",
-    lists: ["links"],
+    page: 'links',
     params: {
       id: 123,
-      tag: "bacon"
+      tag: 'bacon'
     },
     spec: {
-      ".links_holder": {
-        component: "link",
-        lists: ["ids"],
-        filters: ["taggedBacon"],
-        mapper: "idLink"
+      '.links_holder': {
+        component: 'link',
+        lists: ['ids'],
+        filters: ['tagged'],
+        mapper: 'idLink'
       }
     }
-  };
+  }
   var lists = {
     mappers: {
-      idLink: function(item) {
-        return { a: item.id };
+      idLink: function (item) {
+        return { a: item.id }
       }
     },
     filters: {
-      taggedBacon: function(item) {
-        // console.log('tags', item.tags, this.params.tag, item.tags.includes(this.params.tag))
-        if (!item.tags || !this.tag || !this.params.tag) {
-          return false;
+      tagged: function (item) {
+        if (!item.tags || !this.params || !this.params.tag) {
+          return false
         }
-        return item.tags.includes(this.params.tag);
+        return item.tags.includes(this.params.tag)
       },
-      validId: function(item) {
-        return item.id === this.params.id;
+      validId: function (item) {
+        return item.id === this.params.id
       }
     },
     lists: {
       ids: [
-        { id: 123, tags: ["bacon", "cheese"] },
-        { id: 321, tags: ["bacon", "tomato"] },
+        { id: 123, tags: ['bacon', 'cheese'] },
+        { id: 321, tags: ['bacon', 'tomato'] },
         { id: false }
       ]
     }
-  };
+  }
 
-  let renderedComponents;
+  let renderedComponents
   beforeEach(next => {
     loadComponents(page, lists, (err, data) => {
-      if (err) throw err;
-      renderedComponents = data;
-      next();
-    });
-  });
+      if (err) throw err
+      renderedComponents = data
+      next()
+    })
+  })
 
   // it('should fetch the correct compoent')
   // it('should update the compoent with the selectors provided')
@@ -59,17 +57,16 @@ describe("when render components is called", () => {
   // it('should apply the fitlers')
   // it('should apply the ')
 
-  it("should render two components with different html values", () => {
-    console.log(renderedComponents);
-    expect(renderedComponents.link[".links_holder"]).toContain(
+  it('should render two components with different html values', () => {
+    expect(renderedComponents.link['.links_holder']).toContain(
       '<a href="" class="linkage">123</a>'
-    );
-    expect(renderedComponents.link[".links_holder"]).toContain(
+    )
+    expect(renderedComponents.link['.links_holder']).toContain(
       '<a href="" class="linkage">321</a>'
-    );
-  });
+    )
+  })
 
   // it('should render t', () => {
   //   expect(renderedComponents.link['.links_holder']).toEqual({})
   // })
-});
+})
